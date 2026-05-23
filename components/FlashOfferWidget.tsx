da2aeb6926fa7b4a9f830e77d037506f62fe1c86
Ani8909 <singhani5549@gmail.com>
2026-05-23 12:08:06 +0530

    fix: Remove invalid framer motion prop in FlashOfferWidget

--- components/FlashOfferWidget.tsx
+++ components/FlashOfferWidget.tsx
@@ -53,7 +53,7 @@ export default function FlashOfferWidget() {
       <AnimatePresence>
         {isExpanded && (
           <motion.div
-            initial={{ opacity: 0, y: 20, scale: 0.9, originBottomRight: true }}
+            initial={{ opacity: 0, y: 20, scale: 0.9 }}
             animate={{ opacity: 1, y: 0, scale: 1 }}
             exit={{ opacity: 0, scale: 0.9, y: 20 }}
             transition={{ type: "spring", stiffness: 300, damping: 25 }}
